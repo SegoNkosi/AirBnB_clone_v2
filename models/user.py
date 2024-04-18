6ea118f4f271c59eae3e6cc5e969bcbfab7772a1
Lesego Nkosi <nkosilesego8@gmail.com>
2024-04-18 03:18:47 +0200

    function of command interpreter

--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database
+
+    Inherits from SQLAlchemy Base and links to the MySQL table users
+
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = 'users'
+i
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    reviews = relationship('Review', cascade='all, delete', backref='user')
+    places = relationship('Place', cascade='all, delete', backref='user')
